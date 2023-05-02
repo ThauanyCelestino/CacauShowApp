b3530a78a59e7400d8f3348dff4e36931d92d4e1
dieguviana <83664300+dieguviana@users.noreply.github.com>
2023-05-02 16:11:28 -0400

    oii

--- CacauShowApp/RealizarLogin.xaml.cs
+++ CacauShowApp/RealizarLogin.xaml.cs
@@ -23,5 +23,10 @@ namespace CacauShowApp
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
