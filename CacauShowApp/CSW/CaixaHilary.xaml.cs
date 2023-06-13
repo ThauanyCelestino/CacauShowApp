9060611648454107fa8fb3849d14cd4d68caf98a
hilarysoliveira <hilarysoliveira@gmail.com>
2023-06-13 17:25:58 -0400

    add
    add

---
+++ CacauShowApp/CSW/CaixaHilary.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CacauShowApp.CSW
+{
+    /// <summary>
+    /// Lógica interna para CaixaHilary.xaml
+    /// </summary>
+    public partial class CaixaHilary : Window
+    {
+        public CaixaHilary()
+        {
+            InitializeComponent();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void DataGrid_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
